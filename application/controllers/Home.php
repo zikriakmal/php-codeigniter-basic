76530f271c50056c9ba131e60ec82341a2ec203f
zikri <zikriakmale@gmail.com>
2019-08-21 00:12:48 +0700

    inisialisasi projek

---
+++ application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+class Home extends CI_Controller{
+
+    public function index(){
+
+        $tes = array('title'=>'Home');
+            $this->load->view('Home',$tes);
+
+    }
+
+
+}
+
+
+
+
+?>
